bd0cc56a9c6e6f2b913824b6bb4701be1e897ae1
twisti <denizgebauer@web.de>
2025-05-15 18:25:17 +0200

    refactor GeneratorResult.kt: enhance priority-based logic for result combination

--- surf-bitmap-provider-generator/src/main/kotlin/dev/slne/surf/bitmap/generator/generator/GeneratorResult.kt
+++ surf-bitmap-provider-generator/src/main/kotlin/dev/slne/surf/bitmap/generator/generator/GeneratorResult.kt
@@ -1,23 +1,10 @@
 package dev.slne.surf.bitmap.generator.generator
 
-enum class GeneratorResult {
-    FILE_GENERATED,
-    FILE_ALREADY_EXISTS,
-    FILE_NOT_GENERATED;
+enum class GeneratorResult(val priority: Int) {
+    FILE_GENERATED(2),
+    FILE_ALREADY_EXISTS(1),
+    FILE_NOT_GENERATED(0);
 
-    operator fun plus(other: GeneratorResult): GeneratorResult {
-        if (this == FILE_GENERATED && other == FILE_GENERATED) {
-            return FILE_GENERATED
-        }
-
-        if (this == FILE_NOT_GENERATED || other == FILE_NOT_GENERATED) {
-            return FILE_NOT_GENERATED
-        }
-
-        if (this == FILE_ALREADY_EXISTS || other == FILE_ALREADY_EXISTS) {
-            return FILE_ALREADY_EXISTS
-        }
-
-        return FILE_NOT_GENERATED
-    }
+    operator fun plus(other: GeneratorResult): GeneratorResult =
+        if (this.priority <= other.priority) other else this
 }
