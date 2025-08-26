b9698daa062a9b8dd35224d95d4d7bfdb157bb73
Ammo <ammo@slne.dev>
2025-08-26 21:48:52 +0200

    feat: add new clan and rank bitmap providers for Red, Yellow, and Content

---
+++ surf-bitmap-provider-generator/src/main/kotlin/dev/slne/surf/bitmap/generator/command/subcommands/TranslateDefaultCommand.kt
@@ -0,0 +1,49 @@
+package dev.slne.surf.bitmap.generator.command.subcommands
+
+import dev.jorel.commandapi.CommandAPICommand
+import dev.jorel.commandapi.kotlindsl.playerExecutor
+import dev.jorel.commandapi.kotlindsl.subcommand
+import dev.slne.surf.bitmap.bitmaps.Bitmaps
+import dev.slne.surf.surfapi.core.api.messages.adventure.sendText
+import dev.slne.surf.surfapi.core.api.util.mutableObject2ObjectMapOf
+import dev.slne.surf.surfapi.core.api.util.toObjectList
+
+fun CommandAPICommand.translateDefaultCommand() = subcommand("translateDefault") {
+    withPermission("surf.bitmap.generator.command.translate-default")
+
+    playerExecutor { player, args ->
+        val map = mutableObject2ObjectMapOf(
+            "Admin" to Bitmaps.RANK_ADMIN,
+            "Management" to Bitmaps.RANK_MANAGEMENT,
+            "Entwicklung" to Bitmaps.RANK_DEV,
+            "Moderation" to Bitmaps.RANK_SUPPORT,
+            "Support" to Bitmaps.RANK_SUPPORT,
+            "Designer" to Bitmaps.RANK_TEAM_OTHER,
+            "Builder" to Bitmaps.RANK_TEAM_OTHER,
+            "Community" to Bitmaps.RANK_TEAM_OTHER,
+            "Creator" to Bitmaps.RANK_CONTENT,
+            "Contributor" to Bitmaps.RANK_PLAYER_OTHER,
+            "Veteran" to Bitmaps.RANK_PLAYER_OTHER,
+            "Premium+" to Bitmaps.RANK_PREMIUM,
+            "Premium" to Bitmaps.RANK_PREMIUM,
+            "Spieler" to Bitmaps.RANK_PLAYER
+        )
+
+        player.sendText {
+            appendPrefix()
+            info("Alle Ränge: ")
+            appendNewPrefixedLine()
+            appendNewPrefixedLine()
+
+            map.toObjectList().forEachIndexed { index, (key, value) ->
+                if (index != 0) {
+                    appendNewPrefixedLine()
+                }
+
+                variableKey(key)
+                appendSpace()
+                text(value.provider.translateToString(key))
+            }
+        }
+    }
+}
