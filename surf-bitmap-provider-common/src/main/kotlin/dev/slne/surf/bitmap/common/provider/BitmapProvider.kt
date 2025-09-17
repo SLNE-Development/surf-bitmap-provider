d7ae1dcdc3392cb5eefb5a08a14588373070a693
Ammo <ammo@slne.dev>
2025-09-17 19:23:12 +0200

    refactor: streamline BitmapProvider by removing unused methods and simplifying translateToComponent

--- surf-bitmap-provider-common/src/main/kotlin/dev/slne/surf/bitmap/common/provider/BitmapProvider.kt
+++ surf-bitmap-provider-common/src/main/kotlin/dev/slne/surf/bitmap/common/provider/BitmapProvider.kt
@@ -9,9 +9,9 @@ import dev.slne.surf.bitmap.common.utils.Spacing
 import dev.slne.surf.bitmap.common.utils.calculateGlyphSpacing
 import dev.slne.surf.surfapi.core.api.messages.adventure.buildText
 import dev.slne.surf.surfapi.core.api.messages.builder.SurfComponentBuilder
+import net.kyori.adventure.text.Component
 import net.kyori.adventure.text.format.ShadowColor
 import net.kyori.adventure.text.format.TextColor
-import net.kyori.adventure.text.minimessage.MiniMessage
 import java.util.*
 
 /**
@@ -21,14 +21,6 @@ import java.util.*
  */
 object BitmapProvider {
 
-    /**
-     * A list of character providers that are responsible for mapping input characters to their
-     * respective `CharProvider` instances. This collection includes various predefined providers
-     * such as `AlphabetProvider`, `NumberProvider`, `UtilityProvider`, and `UnknownProvider`.
-     *
-     * These providers are used to resolve characters to their appropriate representations,
-     * allowing for flexible and extensible character translation mechanisms.
-     */
     private val providers = LinkedList<Provider>().apply {
         add(AlphabetProvider)
         add(NumberProvider)
@@ -37,23 +29,57 @@ object BitmapProvider {
     }
 
     /**
-     * Translates a single character into its corresponding `CharProvider` using available providers.
+     * Translates the input string into a text component representation, where each character
+     * is mapped to a styled component with the specified foreground and background colors.
      *
-     * @param char The character to be translated into a `CharProvider`.
-     * @return The `CharProvider` corresponding to the given character, or `null` if no match is found.
+     * @param input The string to be translated into a text component.
+     * @param foregroundColor The foreground color to apply to the characters in the text component.
+     * @param backgroundColor The background color to apply behind the characters in the text component.
+     * @param shadowColor The shadow color to apply to the characters in the text component.
+     * @param affixAmount The number of affix characters to add on each side of the text component for padding.
+     *
+     * @return A text component representing the input string with the specified styling.
      */
-    private fun translateOneChar(char: Char) = providers.firstNotNullOfOrNull { it.findChar(char) }
+    fun translateToComponent(
+        input: String,
+        foregroundColor: TextColor,
+        backgroundColor: TextColor,
+        shadowColor: ShadowColor = ShadowColor.none(),
+        affixAmount: Int = 2
+    ): Component {
+        val input = input.lowercase()
+        val translated = translateToCharProviders(input)
+        val background = translated.map { it.generateBackground() }
+        val glyphs = translated.map { it.char }
 
-    /**
-     * Translates an input string into a list of `CharProvider` instances, where each character
-     * in the string is mapped to its corresponding `CharProvider` representation if available.
-     *
-     * @param input The string to be translated into a list of `CharProvider` instances.
-     *              Each character is individually analyzed and potentially mapped.
-     * @return A `LinkedList` of `CharProvider` instances corresponding to the characters
-     *         in the input string. Characters without a matching `CharProvider` are ignored.
-     */
-    fun translateToCharProviders(input: String): LinkedList<CharProvider> {
+        val backgroundString = background.joinToString(Spacing.NEGATIVE_SPACE_ONE.char.toString())
+            .dropLast(2)
+
+        return buildText {
+            append {
+                appendAffix(affixAmount, true)
+                text(backgroundString)
+                appendAffix(affixAmount, false)
+
+                color(backgroundColor)
+            }
+
+            append {
+                text(-translated.sumOf { it.width } - affixAmount)
+                text(glyphs.joinToString(""))
+
+                color(foregroundColor)
+                shadowColor(shadowColor)
+            }
+
+            text(calculateGlyphSpacing(affixAmount))
+        }
+    }
+    
+    private fun translateOneChar(char: Char) =
+        providers.firstNotNullOfOrNull { it.findChar(char) }
+
+    private fun translateToCharProviders(input: String): LinkedList<CharProvider> {
         val list = LinkedList<CharProvider>()
 
         for (char in input) {
@@ -67,133 +93,23 @@ object BitmapProvider {
         return list
     }
 
-    /**
-     * Translates the input string into a text component representation, where each character
-     * is mapped to a styled component with the specified foreground and background colors.
-     *
-     * @param input The string to be translated into a text component.
-     * @param foregroundColor The foreground color to apply to the characters in the text component.
-     * @param shadowColor The shadow color to apply to the characters in the text component. Can be null, then no shadow is applied.
-     * @param backgroundColor The background color to apply behind the characters in the text component.
-     * @param affixAmount The number of affix characters to add on each side of the text component for padding.
-     *
-     * @return A text component representing the input string with the specified styling.
-     */
-    fun translateToComponent(
-        input: String,
-        foregroundColor: TextColor,
-        shadowColor: TextColor?,
-        backgroundColor: TextColor,
-        affixAmount: Int = 2
-    ) = buildText {
-        val input = input.lowercase()
-        val translated = translateToCharProviders(input)
-        val background = translated.map { it.generateBackground() }
-        val glyphs = translated.map { it.char }
-
-        val backgroundString = if (shadowColor != null) {
-            background.joinToString(Spacing.NEGATIVE_SPACE_ONE.char.toString())
-        } else {
-            background.joinToString(Spacing.NEGATIVE_SPACE_ONE.char.toString()).dropLast(2)
-        }
-
-        append {
-            appendAffix(affixAmount, true, backgroundColor)
-            text(backgroundString)
-            appendAffix(affixAmount, false, backgroundColor)
-
-            color(backgroundColor)
-        }
-
-        if (shadowColor != null) {
-            appendGlyphs(glyphs, -translated.sumOf { it.width } - affixAmount, shadowColor)
-        }
-
-        val backshift = if (shadowColor != null) {
-            -translated.sumOf { it.width } - 1
-        } else {
-            -translated.sumOf { it.width } - affixAmount
-        }
-
-        appendGlyphs(glyphs, backshift, foregroundColor)
-
-        val frontshift = if (shadowColor != null) {
-            affixAmount + 1
-        } else {
-            affixAmount
-        }
-
-        text(calculateGlyphSpacing(frontshift))
-        text("<reset>")
-    }
-
-    private fun SurfComponentBuilder.appendGlyphs(
-        glyphs: List<Char>,
-        backshift: Int,
-        color: TextColor,
-    ) {
-        val backshift = calculateGlyphSpacing(backshift)
-
-        append {
-            text(backshift)
-            text(glyphs.joinToString(""))
-
-            color(color)
-            shadowColor(ShadowColor.none())
-        }
-    }
-
     private fun SurfComponentBuilder.appendAffix(
         amount: Int,
         prefix: Boolean,
-        backgroundColor: TextColor
     ) {
         repeat(amount) {
             if (prefix) {
                 append {
                     text(BITMAP_BACKGROUND)
                     text(Spacing.NEGATIVE_SPACE_ONE.char)
-
-                    color(backgroundColor)
                 }
             } else {
                 append {
                     text(Spacing.NEGATIVE_SPACE_ONE.char)
                     text(BITMAP_BACKGROUND)
-
-                    color(backgroundColor)
                 }
             }
         }
     }
-
-    /**
-     * Converts an input string into its serialized representation as a text component,
-     * applying specified foreground and background colors to the characters.
-     *
-     * @param input The input string that will be translated into a serialized string.
-     * @param foregroundColor The color to be applied as the foreground for the text.
-     * @param shadowColor The shadow color to apply to the characters in the text component. Can be null, then no shadow is applied.
-     * @param backgroundColor The color to be applied as the background for the text.
-     * @param affixAmount The number of affix characters to add on each side of the text for padding.
-     *
-     * @return A serialized string representation of the input text with the specified colors.
-     */
-    fun translateToString(
-        input: String,
-        foregroundColor: TextColor,
-        shadowColor: TextColor?,
-        backgroundColor: TextColor,
-        affixAmount: Int = 2
-    ) = MiniMessage.miniMessage()
-        .serialize(
-            translateToComponent(
-                input,
-                foregroundColor,
-                shadowColor,
-                backgroundColor,
-                affixAmount
-            )
-        )
 
 }
