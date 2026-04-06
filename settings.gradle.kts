dae98fcc125714716c9ccdad51c36b1e70da69ac
Ammo <ammo@slne.dev>
2026-04-06 03:37:40 +0200

    chore: update repository URLs and versioning in build configuration

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,13 +3,13 @@ rootProject.name = "surf-bitmap-provider"
 pluginManagement {
     repositories {
         gradlePluginPortal()
-        maven("https://repo.slne.dev/repository/maven-public/") { name = "maven-public" }
+        maven("https://reposilite.slne.dev/public/") { name = "public" }
     }
 }
 
 plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
-    id("dev.slne.surf.api.gradle.settings") version "26.1+"
+    id("dev.slne.surf.api.gradle.settings") version "+"
 }
 
 include("surf-bitmap-provider-common")
